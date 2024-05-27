cf8f99724d6c139c73ac7d84ec05edd6ad727f35
SimanyeSTS <simanyesomdaka@gmail.com>
2024-05-27 10:32:45 +0200

    done

---
+++ script/script.js
@@ -0,0 +1,24 @@
+//1///
+let string = 'Simanye'
+console.log(string)
+console.log(typeof string)
+
+//2///
+
+string = 9
+console.log(string)
+console.log(typeof string)
+
+//3///
+let array = ['Simanye', 'Tyler', 'Somdaka']
+console.log(array)
+console.log(typeof array)
+//////////////////////////////////////////////////////////
+let boolean = true
+console.log(boolean)
+console.log(typeof boolean)
+/////////////////////////////////////////////////////////
+let PGC = undefined
+console.log(PGC)
+console.log(typeof PGC)
+///////////////////////////////////////////////////////
